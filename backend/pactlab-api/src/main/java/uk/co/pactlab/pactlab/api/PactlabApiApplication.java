b25c60ec23490c36dbab8349c4f16c18f421a4d2
sepidehkelishadi <kelishadi.sepide@gmail.com>
2026-03-11 22:12:52 +0000

    feat: add Spring Boot backend service

---
+++ backend/pactlab-api/src/main/java/uk/co/pactlab/pactlab/api/PactlabApiApplication.java
@@ -0,0 +1,13 @@
+package uk.co.pactlab.pactlab.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PactlabApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PactlabApiApplication.class, args);
+	}
+
+}
---
+++ backend/pactlab-api/src/test/java/uk/co/pactlab/pactlab/api/PactlabApiApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.co.pactlab.pactlab.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PactlabApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
